bd17db20d956cc7da66e67b9b8f7085bd8b5258a
Mitch Lloyd <mitch.lloyd@gmail.com>
2015-02-17 09:40:31 -0500

    Document current vehicle endpoint

--- metro.rb
+++ metro.rb
@@ -6,6 +6,8 @@ require 'http'
 require_relative './gtfs-realtime.pb'
 
 TRIP_ENDPOINT = 'http://developer.go-metro.com/TMGTFSRealTimeWebService/TripUpdate/'
+VEHICLE_ENPOINT = 'http://developer.go-metro.com/TMGTFSRealTimeWebService/vehicle'
+
 STOP = 'MADHYDi'
 
 data = HTTP.get(TRIP_ENDPOINT).to_s
