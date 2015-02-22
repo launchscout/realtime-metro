b900f2bb40e3ef554c59aa28d19d37c52d9c182b
Kevin Rockwood <kevin@rockwood.me>
2015-02-22 11:08:48 -0500

    Move connection logic into Metro class

--- lib/metro.rb
+++ lib/metro.rb
@@ -2,3 +2,19 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'metro/arrivals'
+require 'faraday'
+
+class Metro
+  ENDPOINT = 'http://developer.go-metro.com'
+
+  def arrivals
+    response = connection.get("/TMGTFSRealTimeWebService/TripUpdate/")
+    Arrivals.new(response.body)
+  end
+
+  private
+
+  def connection
+    connection ||= Faraday.new(url: ENDPOINT)
+  end
+end
--- server.rb
+++ server.rb
@@ -3,15 +3,10 @@ require 'bundler/setup'
 
 require 'sinatra'
 require 'sinatra/json'
-require 'faraday'
 require 'metro'
 
-METRO_ENDPOINT = 'http://developer.go-metro.com'
-
-connection = conn = Faraday.new(url: METRO_ENDPOINT)
+metro = Metro.new
 
 get '/api/stops/:id' do
-  response = connection.get("/TMGTFSRealTimeWebService/TripUpdate/")
-  arrivals = Arrivals.new(response.body)
-  json arrivals: arrivals.for_stop(params[:id])
+  json arrivals: metro.arrivals.for_stop(params[:id])
 end
